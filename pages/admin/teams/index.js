3a6be67273c157bd86b327c6bb68e19f3279a7b8
Sirima Insorn <sirimainsorn@gmail.com>
2023-09-06 20:11:46 +0700

    add one more team page

---
+++ pages/admin/teams/index.js
@@ -0,0 +1,8 @@
+import SidebarLayout from '@/layouts/SidebarLayout'
+import React from 'react'
+
+export default function TeamsPage() {
+  return <div>TeamsPage</div>
+}
+
+TeamsPage.layout = SidebarLayout
